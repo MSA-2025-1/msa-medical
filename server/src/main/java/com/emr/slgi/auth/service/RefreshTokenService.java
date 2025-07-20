4bb025a9d223ede937898c2d7cfeb08c7e3e1035
Conut-1 <1mim1@naver.com>
2025-07-20 21:54:12 +0900

    tmp

--- server/src/main/java/com/emr/slgi/auth/service/RefreshTokenService.java
+++ server/src/main/java/com/emr/slgi/auth/service/RefreshTokenService.java
@@ -21,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class RefreshTokenService {
+    // TODO: appendonly 설정 알아보기
 
     private final JwtUtil jwtUtil;
     private final StringRedisTemplate stringRedisTemplate;
